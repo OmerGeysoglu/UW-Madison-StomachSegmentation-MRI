da38513ce9ba217acc9172dbb2394c3c34de608c
Ömer Geysoğlu <ogeysoglu@outlook.com>
2024-11-13 03:38:06 +0300

    added custom Dice Loss class

---
+++ utils/metric_utils.py
@@ -0,0 +1,51 @@
+
+import torch
+import torch.nn as nn
+
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=1e-6):
+        '''
+        Initializes the Dice Loss, a measure of similarity between two sets.
+        
+        Args:
+        - smooth (float): Smoothing factor to avoid division by zero.
+        
+        Formula:
+        - Dice Loss = 1 - Dice Score
+        
+        References:
+        - https://oecd.ai/en/catalogue/metrics/dice-score
+        - https://en.wikipedia.org/wiki/Dice-Sørensen_coefficient
+        '''
+        super(DiceLoss, self).__init__()
+        self.smooth = smooth
+    
+    def forward(self, preds, targets):
+        '''
+        Computes the Dice Loss, a measure of similarity between two sets.
+        
+        Args:
+        - preds (torch.Tensor): Predicted segmentation mask (binary or probabilistic tensor).
+        - targets (torch.Tensor): Ground truth segmentation mask (binary tensor).
+        
+        Formula:
+        - Dice Loss = 1 - Dice Score
+        
+        Returns:
+        - torch.Tensor: Dice Loss value.
+        '''
+        
+        # Compute the intersection between predictions and targets
+        intersection = (preds * targets).sum()
+        
+        # Compute the union (sum of all values in both predictions and targets)
+        union = preds.sum() + targets.sum()
+        
+        # Calculate the Dice Score using the formula with a smoothing factor to prevent division by zero
+        dice = (2. * intersection + self.smooth) / (union + self.smooth)
+        
+        # Calculate the Dice Loss as 1 - Dice Score
+        dice_loss = 1. - dice
+        
+        # Return the Dice Loss as a tensor
+        return dice_loss
