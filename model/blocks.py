9526f15afc65d4166a607c41b6aca624b79de3ee
Ömer Geysoğlu <ogeysoglu@outlook.com>
2024-11-15 14:12:48 +0300

    added batch norm and dropout

--- model/blocks.py
+++ model/blocks.py
@@ -3,18 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, dropout_prob: float = 0.0):
         super(DoubleConv, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
-        
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
-        
+        self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_prob) if dropout_prob > 0 else None
         
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
+        x = self.relu(self.bn1(self.conv1(x)))
+        if self.dropout: 
+            x = self.dropout(x)
+        x = self.relu(self.bn2(self.conv2(x)))
+        if self.dropout:
+            x = self.dropout(x)
         return x
 
 class DownSampling(nn.Module):
@@ -42,6 +47,7 @@ class CropAndConcat(nn.Module):
     def forward(self, x1, x2):
         diffY = x1.size()[2] - x2.size()[2]
         diffX = x1.size()[3] - x2.size()[3]
-        x2 = F.pad(x2, (diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2))
+        padding = (diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2)
+        x2 = F.pad(x2, padding)
         x = torch.cat([x2, x1], dim=1)
         return x
--- model/unet.py
+++ model/unet.py
@@ -1,8 +1,9 @@
-import torch
+import os
+import sys
+
 import torch.nn as nn
 import torch.nn.functional as F
-import sys
-import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../model/')))
 from blocks import DoubleConv, DownSampling, UpSampling, CropAndConcat
 
@@ -22,10 +23,10 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         
         self.encoder = nn.ModuleList([
-            DoubleConv(in_channels, 64),
-            DoubleConv(64, 128),
-            DoubleConv(128, 256),
-            DoubleConv(256, 512)    
+            DoubleConv(in_channels, 64, dropout_prob=0.25),
+            DoubleConv(64, 128, dropout_prob=0.5),
+            DoubleConv(128, 256, dropout_prob=0.5),
+            DoubleConv(256, 512, dropout_prob=0.5) 
         ])
         self.down = DownSampling()
         self.middle = DoubleConv(512, 1024)
